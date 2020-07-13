d9e712d96bbad977ec49e0225c85df98848b4837
Andi Bellstedt <bellstedt@gmx.de>
2020-07-13 21:31:20 +0200

    generalizing CI/CD scripts for ease of configuration

--- build/vsts-prerequisites.ps1
+++ build/vsts-prerequisites.ps1
@@ -1,7 +1,44 @@
-$modules = @("Pester", "PSFramework", "PSScriptAnalyzer")
+﻿<#
+    .SYNOPSIS
+        This script ensures prerequisites to run tests
 
-foreach ($module in $modules) {
+    .DESCRIPTION
+        This script ensures prerequisites to run tests
+
+    .PARAMETER ModuleName
+        The name to give to the module.
+
+    .PARAMETER Repository
+        The name of the repository to publish to.
+        Defaults to PSGallery.
+
+#>
+param (
+    [string]
+    $ModuleName,
+
+    [string]
+    $Repository = 'PSGallery',
+
+    $Modules = @("Pester", "PSModuleDevelopment", "PSScriptAnalyzer")
+)
+
+
+# Automatically add missing dependencies
+$data = Import-PowerShellDataFile -Path "$PSScriptRoot\..\$($ModuleName)\$($ModuleName).psd1"
+foreach ($dependency in $data.RequiredModules) {
+    if ($dependency -is [string]) {
+        if ($Modules -contains $dependency) { continue }
+        $Modules += $dependency
+    }
+    else {
+        if ($Modules -contains $dependency.ModuleName) { continue }
+        $Modules += $dependency.ModuleName
+    }
+}
+
+foreach ($module in $Modules) {
     Write-Host "Installing $module" -ForegroundColor Cyan
-    Install-Module $module -Force -SkipPublisherCheck
+    Install-Module $module -Force -SkipPublisherCheck -Repository $Repository
     Import-Module $module -Force -PassThru
 }
